eb0eb581ff72864e19bb677bc5a7299fee2d3473
Halil ÇAKAR <hcakar.1992@gmail.com>
2018-09-26 00:22:44 +0300

    Creating Custom Components

--- App.js
+++ App.js
@@ -1,46 +1,27 @@
 import React, { Component } from 'react';
-import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import PlaceInput from './src/components/PlaceInput';
+import PlaceList from './src/components/PlaceList';
 
 export default class App extends Component {
     state = {
-		placeName: '',
 		places: []
     };
 
-    placeNameChangedHandler = event => {
-        this.setState({ placeName: event });
-	};
-	
-	placeSubmitHandler = () => {
-		if(this.state.placeName.trim() === '') { return; }
-
-		this.setState(prevState => {
+	placeAddedHandler = (placeName) => {
+		this.setState((prevState) => {
 			return {
-				placeName: '',
-				places: prevState.places.concat(prevState.placeName)
-			}
+				places: prevState.places.concat(placeName)
+			};
 		});
 	};
 
     render() {
-		const placesOutput = this.state.places.map((place, i) => (<Text key={i}>{place}</Text>));
         return (
 			<View style={styles.container}>
-				<View style={styles.inputContainer}>
-					<TextInput 
-						style={styles.textInput} 
-						placeholder={"An awesome place!"} 
-						value={this.state.placeName} 
-						onChangeText={this.placeNameChangedHandler} />
-					<Button 
-						title={"Add"} 
-						style={styles.placeButton}
-						onPress={this.placeSubmitHandler}
-					/>
-				</View>
-				<View style={{marginTop: 30}}> 
-					{placesOutput}
-				</View>
+				<PlaceInput onPlaceAdded={this.placeAddedHandler} />
+				<PlaceList places={this.state.places} />
 			</View>
 		);
     }
@@ -50,23 +31,8 @@ const styles = StyleSheet.create({
 	container: {
         flex: 1,
 		padding: 26,
-		backgroundColor: "#F5FCFF",
-		justifyContent: "flex-start",
-		alignItems: "center"
-	},
-	inputContainer: {
-		width: '100%',
-		flexDirection: "row",
-		justifyContent: "space-between",
+		backgroundColor: '#F5FCFF',
+		justifyContent: 'flex-start',
 		alignItems: 'center'
-	},
-	textInput: {
-		height: 40,
-		width: '70%',
-		borderBottomWidth: 1,
-		borderColor: '#ddd',
-	},
-	placeButton: {
-		width: '30%'
 	}
 });
