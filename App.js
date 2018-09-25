105d7e09267cebf0313ea17fca0189caf4cca89a
Halil ÇAKAR <hcakar.1992@gmail.com>
2018-09-25 22:57:58 +0300

    Adding a Button and Managing State

--- App.js
+++ App.js
@@ -1,40 +1,72 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from 'react';
+import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    state = {
+		placeName: '',
+		places: []
+    };
+
+    placeNameChangedHandler = event => {
+        this.setState({ placeName: event });
+	};
+	
+	placeSubmitHandler = () => {
+		if(this.state.placeName.trim() === '') { return; }
+
+		this.setState(prevState => {
+			return {
+				placeName: '',
+				places: prevState.places.concat(prevState.placeName)
+			}
+		});
+	};
+
+    render() {
+		const placesOutput = this.state.places.map((place, i) => (<Text key={i}>{place}</Text>));
+        return (
+			<View style={styles.container}>
+				<View style={styles.inputContainer}>
+					<TextInput 
+						style={styles.textInput} 
+						placeholder={"An awesome place!"} 
+						value={this.state.placeName} 
+						onChangeText={this.placeNameChangedHandler} />
+					<Button 
+						title={"Add"} 
+						style={styles.placeButton}
+						onPress={this.placeSubmitHandler}
+					/>
+				</View>
+				<View style={{marginTop: 30}}> 
+					{placesOutput}
+				</View>
+			</View>
+		);
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+        flex: 1,
+		padding: 26,
+		backgroundColor: "#F5FCFF",
+		justifyContent: "flex-start",
+		alignItems: "center"
+	},
+	inputContainer: {
+		width: '100%',
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: 'center'
+	},
+	textInput: {
+		height: 40,
+		width: '70%',
+		borderBottomWidth: 1,
+		borderColor: '#ddd',
+	},
+	placeButton: {
+		width: '30%'
+	}
 });
